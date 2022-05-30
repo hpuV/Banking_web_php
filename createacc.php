c337fba1461bcd58f635d882db28636c8493a26d
hpuV <andy1105yo@gmail.com>
2022-05-30 22:48:25 +0800

    [doc]createacc CSS style

--- createacc.php
+++ createacc.php
@@ -6,161 +6,125 @@ session_start();
 
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true && $_SESSION['level']>=2){
 ?>
-<DOCTYPE html>
+<!doctype html>
 <html>
 <head>
-<title>新增會員</title>
-<link href="user.css" rel="stylesheet" type="text/css">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content..="chrome=1">
+<meta name="viewport" content..="width=device-width, initial-scale=1">
+<title>銀行網站</title>
 <script type="text/javascript">
-  function showOrHide(){
-    var dis = document.getElementById('mapid')
+	function showOrHide(){
+		var dis = document.getElementById('mapid');
 
-    if(dis.style.display == ''){
-     dis.style.display = 'none';
-    }else{
-      dis.style.display = '';
-    }
-  }
-            
+		if(dis.style.display == ''){
+			dis.style.display = 'none';
+		}else{
+			dis.style.display = '';
+		}
+	}
 </script>
+<link href="css/createaccstyle.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+</style>
 </head>
 <body>
- <table>
-  <tr class="headbar">
-    <td>歡迎<?php
-             echo $_SESSION['nickname']; 
-           ?>(<a href="logout.php" class="logout">登出</a>)
-    </td>
-    <td><a href="usercenter.php" class="headStr">個人資料</a></td>
-    <td><a href="useradminmanage.php" class="headStr">會員管理</a></td>
-    <td class="headNow">
-      <a href="createacc.php" class="headStr">新增會員</a>
-    <td><a href="searchresult.php?">搜尋會員資料</a></td>
-    </td>
-  </tr>
- </table><br /><br />
-<?php
-  }else{
-     echo "非法登入!";
-     exit();
+<div class="container">
+  <header>
+	 <nav class="primary_header" id="menu">
+      <ul>
+        <a class="h1title">Banking</a>
+        <li><a class="nav-link" href="mainpage.php">首頁</a></li>
+        <li><a class="nav-link" href="goldprice.php">黃金價格</a></li>
+        <li><a class="nav-link" href="stockprice.php">股票價格</a></li>
+        <li><a class="nav-link" href="userdeter.php">會員中心</a></li>
+        <li><a class="nav-link" href="statementsearch.php">收支查詢</a></li>
+		    <li><a class="nav-link" href="logout.php">登出</a></li>
+      </ul>
+    </nav>
+  </header>
+  <section>
+	<div class="top-box"></div>
+	<article class="left_article">
+      <h3 class="toph3"><a class="nav-info" href="useradmincenter.php">個人資料</a></h3>
+      <h3><a class="nav-info" href="editacc.php">變更資料</a></h3>
+      <h3><a class="nav-info" href="editpwd.php">更改密碼</a></h3>
+		  <h3><a class="nav-info" href="useradminmanage.php">會員管理</a></h3>
+		  <h3><a class="nav-info" href="createacc.php">新增會員</a></h3>
+		  <h3><a class="nav-info" href="searchresult.php">查詢會員</a></h3>
+  </article>
+  <aside class="right_article">
+  <?php
+    }else{
+      echo "非法登入!";
+      exit();
+    }
+
+  $settime=strtotime('+7 hours');
+  $gettime=date('YmdHis',$settime);
+
+  if(!empty($_POST['m_username'])){
+    $in_username= $_POST['m_username'];
+    $in_password= $_POST['m_password'];
+    $in_nickname=$_POST['m_nick'];
+    $in_level= $_POST['m_level'];
+    $in_gender= $_POST['m_gender'];
+    $in_bday= $_POST['m_bday'];
+    $in_email= $_POST['m_email'];
+    $in_phone= $_POST['m_phone'];
+    $in_address= $_POST['m_address'];
+
+    $in_account = rand(10,99999999);
+    $in_account = sprintf("%09d",$in_account);
+
+      $sql="INSERT INTO memberdata 
+            VALUE(NULL,'$in_account','$in_username','$in_password','$in_nickname','$in_level','$in_gender','$in_bday','$in_email','$in_phone','$in_address');";
+      mysqli_query($db_link,$sql);
   }
-
-$settime=strtotime('+7 hours');
-$gettime=date('YmdHis',$settime);
-
-if(!empty($_POST['m_username'])){
-  $in_username= $_POST['m_username'];
-  $in_password= $_POST['m_password'];
-  $in_nickname=$_POST['m_nick'];
-  $in_level= $_POST['m_level'];
-  $in_gender= $_POST['m_gender'];
-  $in_bday= $_POST['m_bday'];
-  $in_email= $_POST['m_email'];
-  $in_phone= $_POST['m_phone'];
-  $in_address= $_POST['m_address'];
-
-  $in_account = rand(10,99999999);
-  $in_account = sprintf("%09d",$in_account);
-
-    $sql="INSERT INTO memberdata 
-          VALUE(NULL,'$in_account','$in_username','$in_password','$in_nickname','$in_level','$in_gender','$in_bday','$in_email','$in_phone','$in_address');";
-    mysqli_query($db_link,$sql);
-}
-?>
-<form action="" method="post"  enctype="multipart/form-data">
-<div class="form">
-  <tr>
-    <td class="title">性別：</td> 
-      <td class="content">
-        <select name="m_gender" />
-          <option value="男">男性</option>
-          <option value="女">女性</option>
-          <option value="其他">其他</option>
-        </select><br/><br/>
-      </td>
-    </tr>
-  <tr>
-    <td class="title">生日：</td> 
-      <td class="content">
-        <input type="date" id="bday" name="m_bday" value=""><br/><br/>
-      </td>
-  </tr>
-  <tr>
-    <td class="title">Email: </td> 
-    <td class="content">
-      <input name="m_email" type="text"  value="" /><br/><br/>
-    </td>
-  </tr>
-  <tr>
-    <td class="title">電話: </td> 
-    <td class="content">
-      <input name="m_phone" type="text"  value="" /><br/><br/>
-    </td>
-  </tr>
-  <tr>
-    <td class="title">住址：</td> 
-      <td class="content">
-        <input type="text" name="m_address" value="">
-          <a href="javascript:;" onclick="showOrHide()">查詢地圖</a><br/><br/>
-      </td>
-  </tr>
-  <tr>
-    <td class="title">帳號: </td>
-    <td class="content">
-       <input name="m_username" type="text" value="" /><br/><br/>
-    </td>
-  </tr>
-  <tr>
-    <td class="title">密碼</td>
-    <td class="content"><input name="m_password" type="password" value="" /></td><br/><br/>
-  </tr>
-  <tr>
-    <td class="title">暱稱: </td> 
-    <td class="content">
-      <input name="m_nick" type="text"  value="" /><br/><br/>
-    </td>
-  </tr>
-  <tr>
-    <td class="title">等級</td>
-    <td class="content">
-      <select name="m_level" />
-        <option value="0">無</option>
-        <option value="1">用戶</option>
-        <option value="2">管理者</option>
-      </select><br/><br/>
-    </td>
-  </tr>
-  <tr>
-    <td colspan="2" style="text-align:center;">
-      <input name="" type="submit" value="確認新增" />
-    </td>
-  </tr>
-  </div>
-</form>
-<form>
-  <div id="mapid" style="display: none" class="map">
-    <iframe
-      src=https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3681.674937442897!2d120.50265221479374!3d22.665905285134848!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e178cae0b7a29%3A0x50bf59e1705a0a73!2z5ZyL56uL5bGP5p2x5aSn5a245rCR55Sf5qCh5Y2A!5e0!3m2!1szh-TW!2stw!4v1651384709604!5m2!1szh-TW!2stw"
-      width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
-      <!-- 嵌入 Google map -->
-  </div>
-</form>
-<style>
-  .map{
-    float: left;
-    width:600px;
-    line-height:50px;
-    padding:20px;
-  }
-  .form{
-    display: inline-block;
-    width: 350px;        
-    line-height: 20px;
-    padding:20px;
-    float: left;
-    margin-right:10px;
-  }
-</style>
+  ?>
+  <form action="" method="post" enctype="multipart/form-data">
+	<div class="bg-style1" href="#">
+   	    <h1>新增會員</h1>
+		  <h2 class="lbl1">帳號</h2>
+		  <input type="text" name="username" class="txt">
+		  <h2 class="lbl1">密碼</h2>
+		  <input type="password" name="username" class="txt">
+		  <h2 class="lbl2">等級</h2>
+		  <select name="m_level" class="select">
+          <option value="0">無</option>
+          <option value="1">用戶</option>
+          <option value="2">管理者</option>
+          </select>
+		  <h2 class="lbl1">名稱</h2>
+		  <input type="text" name="nickname" id="nickname" class="txt">
+		  <h2 class="lbl2">性別</h2>
+		  <select name="gender" class="select">
+          	<option value="男">Male</option>
+          	<option value="女">Female</option>
+          	<option value="其他">Other</option>
+          </select>
+		  <h2 class="lbl1">生日</h2>
+		  <input type="date" id="bday" name="bday" class="day">
+		  <h2 class="lbl1">電子信箱</h2>
+		  <input type="text" name="email" class="txt">
+		  <h2 class="lbl1">手機號碼</h2>
+		  <input type="text" name="phone" class="txt">
+		  <h2 class="lbl1">地址</h2>
+		  <input type="text" name="address" class="txtmap">
+		  <a href="javascript:;" class="map-link" onclick="showOrHide()">搜尋地址</a>
+		  <input type="submit" value="新增會員" name="submit" class="btn">
+       </div>
+	 <div class="clearfix"></div>
+	  <div id="mapid" style="display: none" class="map">
+		  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29455.80915183944!2d120.47031454449525!3d22.65467805683902!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e178cae0b7a29%3A0x50bf59e1705a0a73!2z5ZyL56uL5bGP5p2x5aSn5a245rCR55Sf5qCh5Y2A!5e0!3m2!1szh-TW!2stw!4v1652279981894!5m2!1szh-TW!2stw" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+		  <h3>點選「搜尋地址」關閉Google地圖</h3>
+      </div>
+    </aside>
+	</section>
+  <footer class="tertiary_header footer">
+    <div class="copyright">Copyright &copy;<strong> Chin-An Liu.</strong> All rights reserved.</div>
+  </footer>
+</div>
 </form>
 </body>
 </html>
