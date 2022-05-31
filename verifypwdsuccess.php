69997b08c9cd76da1b37638c3cd0ea28e0956379
hpuV <andy1105yo@gmail.com>
2022-06-01 00:10:30 +0800

    [doc]verifypwd CSS style update1.0 & registerpage update

---
+++ verifypwdsuccess.php
@@ -0,0 +1,31 @@
+<?php
+  header("Refresh: 10; url=logout.php");
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Banking</title>
+<link href="css/registernextpagestyle.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="container">
+  <div class="cssload-loader">
+		<div class="cssload-inner cssload-one"></div>
+		<div class="cssload-inner cssload-two"></div>
+		<div class="cssload-inner cssload-three"></div>
+	  </div>
+  <section>
+    <article class="succ_article">
+	  <div class="bg-style1" href="#">
+		  <h1 class="lbl1">更改成功!</h1>
+		  <h2>3秒後將自動跳轉頁面</h2>
+		  <h2 class="lbl2"><a href="logout.php" class="lbl-link">未成功跳轉頁面請點擊此</a></h2>
+       </div>
+    </article>
+	</section>
+</div>
+</body>
+</html>
