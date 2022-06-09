810194f30dbb9d4ea11d28032a9bda19a3507bd6
hpuV <andy1105yo@gmail.com>
2022-06-09 21:49:29 +0800

    [doc]icon add 2.1

--- editacc.php
+++ editacc.php
@@ -16,7 +16,9 @@ $row_Login = mysqli_fetch_assoc($result);
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content..="width=device-width, initial-scale=1">
-<title>修改會員資料</title>
+<title>ATBC Banking</title>
+<link rel="icon" href="img/ATBClogo.ico" type="image/x-icon">
+<link rel="shortcut icon" href="img/ATBClogo.ico" type="image/x-icon">
 <script type="text/javascript">
 	function showOrHide(){
 		var dis = document.getElementById('mapid');
